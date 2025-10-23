174480765c2c5c8d8cd3fc3a8aee77f5c665fec8
maullidya <zhramldy@gmail.com>
2025-10-23 08:58:29 +0700

    siakad for

---
+++ siakadFor15.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class siakadFor15 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       double nilai, tertinggi, terendah;
+       tertinggi = 0;
+       terendah = 100;
+       for (int i = 1; i <= 10; i++)
+       {
+        System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+        nilai = sc.nextDouble();
+        if (nilai > tertinggi) {
+            tertinggi = nilai;
+        }
+        if (nilai < terendah) {
+            terendah = nilai;
+        }
+       }
+       System.out.println("Nilai Tertinggi: " + tertinggi);
+       System.out.println("Nilai Terendah: " + terendah);
+       
+    }
+}
