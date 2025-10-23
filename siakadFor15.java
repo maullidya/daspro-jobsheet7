a92087eaa24bec2e2461fbcc79b417435d5ec444
maullidya <zhramldy@gmail.com>
2025-10-23 13:32:36 +0700

    modifikasi

--- siakadFor15.java
+++ siakadFor15.java
@@ -4,8 +4,9 @@ public class siakadFor15 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        double nilai, tertinggi, terendah;
-       tertinggi = 0;
-       terendah = 100;
+       int lulus=0, tidakLulus=0;
+       tertinggi = 100;
+       terendah = 0;
        for (int i = 1; i <= 10; i++)
        {
         System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -16,9 +17,18 @@ public class siakadFor15 {
         if (nilai < terendah) {
             terendah = nilai;
         }
+
+        if (60 > nilai) {
+        lulus++;
+       }else {
+        tidakLulus++;
        }
+       }
+
+       
        System.out.println("Nilai Tertinggi: " + tertinggi);
        System.out.println("Nilai Terendah: " + terendah);
-       
+       System.out.println("Jumlah Mahasiswa Lulus: " + lulus);
+       System.out.println("Jumlah Mahasiswa Tidak Lulus: " + tidakLulus);
     }
 }
